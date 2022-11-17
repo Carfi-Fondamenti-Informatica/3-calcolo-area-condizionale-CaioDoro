9f59993eeae8b73e7273f9228bf53c77d6f6ad47
CaioDoro <117940103+CaioDoro@users.noreply.github.com>
2022-11-17 16:49:00 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
-   return 0;
+int main(){
+    float a,b, At, Aq, Ar;
+    int i;
+    cin >> a >> b >> i ;
+    switch (i) {
+        case 1:
+            i=0;
+            At= (a*b)/2;
+            cout << At;
+            break;
+        case 2:
+            i=1;
+            Aq=a*a;
+            cout << Aq;
+            break;
+        case 3:
+            i=2;
+            Ar=a*b;
+            cout << Ar;
+            break;
+        default:
+            cout << "opzione non valida";
+
+    }
+    return 0;
+
 }
