0351388a591cf1b832fbb763929f34f4ca3e2981
CaioDoro <117940103+CaioDoro@users.noreply.github.com>
2022-11-17 16:54:21 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,17 @@
-#include <iostream>
-
-using namespace std;
-
 int main(){
     float a,b, At, Aq, Ar;
     int i;
     cin >> a >> b >> i ;
     switch (i) {
-        case 1:
-            i=0;
+        case 0:
             At= (a*b)/2;
             cout << At;
             break;
-        case 2:
-            i=1;
+        case 1:
             Aq=a*a;
             cout << Aq;
             break;
-        case 3:
-            i=2;
+        case 2:
             Ar=a*b;
             cout << Ar;
             break;
